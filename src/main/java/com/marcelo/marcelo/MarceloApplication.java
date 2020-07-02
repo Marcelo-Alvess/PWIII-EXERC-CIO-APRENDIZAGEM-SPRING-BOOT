f82e8ddda021f162338ee520ce7f844835de60d7
Marcelo-Alvess <marcelo.alves.sousa@outlook.com>
2020-07-02 18:31:43 -0300

    Exercício SPRING BOOT

---
+++ src/main/java/com/marcelo/marcelo/MarceloApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.marcelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarceloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarceloApplication.class, args);
+	}
+
+}
